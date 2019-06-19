6ad8d7f43a91efd5753b80894ce0f9baea49df7f
zhoupengliang <darren_77@163.com>
2019-06-19 18:34:57 +0800

    helloworld commit

---
+++ src/main/java/cn/baiying/HellowoldApplication.java
@@ -0,0 +1,10 @@
+package cn.baiying;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowoldApplication {
+    public static void main(String[] args) {
+
+    }
+}
